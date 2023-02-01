f999068ed63ada4ef440bef20bbde1abc2aa46c0
David Ramirez <dframirez.usmc@gmail.com>
2023-02-01 09:54:02 -0700

    Update __init__.py

--- bundle_adjust/__init__.py
+++ bundle_adjust/__init__.py
@@ -2,9 +2,9 @@ import sys
 
 import numpy as np
 
-from bundle_adjust import ba_timeseries, loader
+from my_bundle_adjust import ba_timeseries, loader
 
-__version__ = "0.1.0dev"
+__version__ = "my_0.1.0"
 
 
 def main(config_path):
